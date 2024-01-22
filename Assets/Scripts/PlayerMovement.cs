2fdf1d0b928e7e2a9e92cf07523e59ce057804c4
Evan Roberts <eroberts1998@icloud.com>
2024-01-22 16:43:16 -0500

    started initial movement logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector2 moveInput;
+    Rigidbody2D rigidbody;
+    [SerializeField] float runSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    void OnMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void Run(){
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, rigidbody.velocity.y);
+        rigidbody.velocity = playerVelocity;
     }
 }
