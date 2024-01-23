5dc4a64973f289e81bdd94bb156ee3aea29dcf2c
Evan Roberts <eroberts1998@icloud.com>
2024-01-23 11:09:23 -0500

    started logic for climbing logic, have to stop sliding down and fix animation so its paused when not moving on later instead of reverting back to idle animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,18 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D rigidbody;
     Animator animator;
+    CapsuleCollider2D capsuleCollider;
 
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
@@ -24,12 +28,37 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (value.isPressed)
+        {
+            rigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ladder"))) {
+            animator.SetBool("isClimbing", false);
+            return;
+        }
+        Vector2 climbVelocity = new Vector2(rigidbody.velocity.x, moveInput.y * climbSpeed);
+        rigidbody.velocity = climbVelocity;
+        bool playerHasVerticalSpeed = Mathf.Abs(rigidbody.velocity.y) > Mathf.Epsilon;
+        animator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 
     void Run()
