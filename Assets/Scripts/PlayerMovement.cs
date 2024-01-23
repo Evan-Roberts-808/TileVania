d6ddd9a99783f8d86b3b55856e6835b12df1b8c4
Evan Roberts <eroberts1998@icloud.com>
2024-01-23 09:25:46 -0500

    finish running logic to have sprite flip based on direction and switch between idle and running animations properly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,27 +8,46 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D rigidbody;
+    Animator animator;
+
     [SerializeField] float runSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
-    void OnMove(InputValue value) {
+    void OnMove(InputValue value)
+    {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
     }
 
-    void Run(){
-        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed, rigidbody.velocity.y);
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidbody.velocity.y);
         rigidbody.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerHasHorizontalSpeed);
+
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody.velocity.x), 1f);
+        }
     }
 }
